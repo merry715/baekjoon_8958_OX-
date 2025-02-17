cd65fe0de023988670183f59a0f779f060c87b0c
mjeon <mjeon@MERRY>
2025-02-17 18:23:55 +0900

    초기 커밋.

---
+++ 8958_250217/8958_250217.cpp
@@ -0,0 +1,31 @@
+﻿#include <stdio.h>
+#pragma warning (disable: 4996)
+#include <string.h>
+
+int main(void)
+{
+	char str[80];
+
+	scanf("%s", str);
+	int k = strlen(str);
+
+	int result = 0;
+	int count = 0;
+
+	for (int i = 0; i < k; i++)
+	{
+		if (str[i] == 'o')
+		{
+			count = count + 1;
+			result = result + count;
+		}
+		else
+		{
+			count = 0;
+		}
+	}
+
+	printf("%d", result);
+
+	return 0;
+}
